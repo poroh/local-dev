43b3cdf8919e27284713f44a1fc66d79e86ab192
Dmitry Poroh <dporoh@pm.me>
2025-08-03 15:45:29 -0700

    better dependency vars naming

--- ldv-dep.mk
+++ ldv-dep.mk
@@ -32,29 +32,29 @@ define ldv-dep.f-define
 endef
 
 # All defined dependencies
-ldv-dep.all = $(foreach d,$(ldv-dep..all),$(ldv-dep..$d))
+ldv-dep.all = $(foreach d,$(ldv-dep..all),$(ldv-dep..[$d].file))
 
 # Target and prerequisites for dependency. Pattern here is:
 # $(call ldv-dep.f-target,something): $(call ldv-dep.f-prereq,something)
 # 	your-rules
 #       $(call ldv-dep.f-touch,$@)
-ldv-dep.f-target = $(call ldv-vars.f-get,ldv-dep..$1,Dep $1 is not defined)
-ldv-dep.f-prereq = $(ldv-dep..root) $(foreach d,$(ldv-dep..ldv-deps-$1),$(ldv-dep..$d))
+ldv-dep.f-target = $(call ldv-vars.f-get,ldv-dep..[$1].file,Dep $1 is not defined)
+ldv-dep.f-prereq = $(ldv-dep..root) $(foreach d,$(ldv-dep..[$1].ldv-deps),$(ldv-dep..[$d].file))
 ldv-dep.f-touch = $(call ldv-bin.f-exec,touch) $1
 
-ldv-dep.f-sha = $(call ldv-vars.f-get,ldv-dep..shasum-dep-$1,Dep $1 is not defined)
+ldv-dep.f-sha = $(call ldv-vars.f-get,ldv-dep..[$1].shasum-dep,Dep $1 is not defined)
 
 # ================================================================================
 # Implementation
 
 define ldv-dep..f-define-vars-shasums
-  $(foreach d,$3,$(call ldv-vars.f-must-not-empty,ldv-dep..$(d),Dependency $d is not defined))
-  ldv-dep..shasum-dep-$1 := $(call ldv-shasum-f-short,$2 $(foreach d,$3,$(ldv-dep..shasum-dep-$(d))))
-  ldv-dep..ldv-deps-$1 := $3
+  $(foreach d,$3,$(call ldv-vars.f-must-not-empty,ldv-dep..[$d].file,Dependency $d is not defined))
+  ldv-dep..[$1].shasum-dep := $(call ldv-shasum-f-short,$2 $(foreach d,$3,$(ldv-dep..[$d].shasum-dep)))
+  ldv-dep..[$1].ldv-deps := $3
 endef
 
 define ldv-dep..f-define-vars-2
-  ldv-dep..$1 := $(ldv-dep.base-path)/$1.$(ldv-dep..shasum-dep-$1).ldv-dep
+  ldv-dep..[$1].file := $(ldv-dep.base-path)/$1.$(ldv-dep..[$1].shasum-dep).ldv-dep
   ldv-dep..all += $1
 endef
 
